001518454e39fd901506f64d370148134421eb1a
Dmitry <rasma1@mail.ru>
2024-11-30 13:59:59 +0300

    Homework4

--- src/main/java/org/geekbrains/Lesson3/Homework/Task2/Person.java
+++ src/main/java/org/geekbrains/Lesson3/Homework/Task2/Person.java
@@ -6,6 +6,8 @@ public class Person implements Serializable {
     private String name;
     private int age;
 
+
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
